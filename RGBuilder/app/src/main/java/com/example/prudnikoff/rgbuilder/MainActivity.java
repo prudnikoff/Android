3385c35516da405a2e2f8a8292e392125cdbd672
prudnikoff <prudnikoff@vproshloe.com>
2017-07-03 03:16:31 -0100

    RGBuilder has been added

---
+++ RGBuilder/app/src/main/java/com/example/prudnikoff/rgbuilder/MainActivity.java
@@ -0,0 +1,168 @@
+package com.example.prudnikoff.rgbuilder;
+
+import android.graphics.Color;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText editRed = null;
+    private EditText editGreen= null;
+    private EditText editBlue = null;
+    private RelativeLayout mainLayout = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_activity);
+        mainLayout = (RelativeLayout)findViewById(R.id.relativeLayoutMain);
+        editRed = (EditText)findViewById(R.id.editTextRed);
+        editGreen = (EditText)findViewById(R.id.editTextGreen);
+        editBlue = (EditText)findViewById(R.id.editTextBlue);
+        editRed.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+                setMainColor();
+
+            }
+        });
+
+        editGreen.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+                setMainColor();
+
+            }
+        });
+
+        editBlue.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+                setMainColor();
+
+            }
+        });
+        mainLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setRandomColor();
+            }
+        });
+
+    }
+
+    @Override protected void onStart() {
+        super.onStart();
+        setMainColor();
+    }
+
+    public int getRandomColor(){
+        Random rnd = new Random();
+        return Color.argb(255, rnd.nextInt(256), rnd.nextInt(256), rnd.nextInt(256));
+    }
+
+    private void setRandomColor() {
+        Random rnd = new Random();
+        int red = rnd.nextInt(255);
+        int green = rnd.nextInt(255);
+        int blue = rnd.nextInt(255);
+        int randomColor = Color.rgb(red, green, blue);
+        editRed.setText(Integer.toHexString(red));
+        editGreen.setText(Integer.toHexString(green));
+        editBlue.setText(Integer.toHexString(blue));
+        mainLayout.setBackgroundColor(randomColor);
+    }
+
+    private boolean checkTypedColor(String color) {
+
+        final String allowed = "ABCDEFabcdef0123456789";
+        boolean result = true;
+
+        if (color.length() > 2 | color.length() < 1) {
+            result = false;
+        }
+
+        for (int i = 0; i < color.length() && result; i++) {
+            if (!allowed.contains(color.charAt(i) + "")) {
+                result = false;
+            }
+        }
+        return result;
+    }
+
+    private void setMainColor() {
+        String sRed = editRed.getText().toString();
+        String sGreen = editGreen.getText().toString();
+        String sBlue = editBlue.getText().toString();
+        int red = 0;
+        int green = 0;
+        int blue = 0;
+        if (checkTypedColor(sRed)) {
+            red = Integer.parseInt(sRed, 16);
+        } else {
+            if (editRed.getText().length() > 0) {
+                editRed.setText("00");
+            }
+        }
+        if (checkTypedColor(sGreen)) {
+            green = Integer.parseInt(sGreen, 16);
+        } else {
+            if (editGreen.getText().length() > 0) {
+                editGreen.setText("00");
+            }
+        }
+        if (checkTypedColor(sBlue)) {
+            blue = Integer.parseInt(sBlue, 16);
+        } else {
+            if (editBlue.getText().length() > 0) {
+                editBlue.setText("00");
+            }
+        }
+        int mainColor = Color.rgb(red, green, blue);
+        mainLayout.setBackgroundColor(mainColor);
+
+    }
+}
