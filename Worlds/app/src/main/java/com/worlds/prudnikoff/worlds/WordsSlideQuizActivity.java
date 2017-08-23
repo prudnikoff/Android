d4ee54d191c76642a0c32ae016c66255799db0ad
prudnikoff <prudnikoff@vproshloe.com>
2017-08-23 03:21:12 +0300

    Worlds 1.5, all cardViews redesign

---
+++ Worlds/app/src/main/java/com/worlds/prudnikoff/worlds/WordsSlideQuizActivity.java
@@ -0,0 +1,17 @@
+package com.worlds.prudnikoff.worlds;
+
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+
+public class WordsSlideQuizActivity extends FragmentActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.words_slide_quiz_activity);
+
+    }
+
+
+}
