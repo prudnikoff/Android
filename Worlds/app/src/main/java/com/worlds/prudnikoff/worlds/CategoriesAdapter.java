ded247df8ecd59afcd0000a9c0f569b1564ac71c
prudnikoff <prudnikoff@vproshloe.com>
2017-09-03 21:26:06 +0300

    README.md was updated

--- Worlds/app/src/main/java/com/worlds/prudnikoff/worlds/CategoriesAdapter.java
+++ Worlds/app/src/main/java/com/worlds/prudnikoff/worlds/CategoriesAdapter.java
@@ -6,6 +6,7 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageButton;
 import android.widget.TextView;
@@ -64,6 +65,7 @@ class CategoriesAdapter extends RecyclerView.Adapter<CategoriesAdapter.CategoryH
             starButton = (ImageButton)itemView.findViewById(R.id.star_button);
             cardView = (CardView)itemView.findViewById(R.id.category_cardView);
             cardView.setOnClickListener(this);
+            cardView.setOnLongClickListener(this);
             starButton.setOnClickListener(this);
         }
 
@@ -81,8 +83,8 @@ class CategoriesAdapter extends RecyclerView.Adapter<CategoriesAdapter.CategoryH
                 cardView.startAnimation(AnimationUtils.loadAnimation(v.getContext(),
                         android.R.anim.slide_in_left));
                 MainActivity.notifyAboutCategoriesChanging();
-                cardView.startAnimation(AnimationUtils.loadAnimation(v.getContext(),
-                        android.R.anim.slide_in_left));
+                Animation animation = AnimationUtils.loadAnimation(v.getContext(), android.R.anim.slide_in_left);
+                cardView.startAnimation(animation);
             }
             if (v.getId() == cardView.getId()) {
                 Intent intent = new Intent(v.getContext(), CategoryWordsActivity.class);
